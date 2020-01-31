2ae9239764a1f944ca9f54f1a74abaaaaaa7e4ad
Emily <emily.fang@duke.edu>
2020-01-31 12:40:51 -0500

    Can check list of temperature for fever

--- chol_analysis.py
+++ chol_analysis.py
@@ -54,6 +54,13 @@ def interface():
                 elif choice == "1":
                         cholesterol_analysis()
 
+def fever_check(temp_list):
+        fever = False
+        for temperature in temp_list:
+                if temperature > 98.6:
+                        fever = True
+        return fever
+
 
 if __name__ == "__main__":
         interface()
